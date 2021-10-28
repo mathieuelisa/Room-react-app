fc68a6ba8c93cf0bf1946dd812ae1bfe24ecaa43
mathieuelisa <mathieu.elisa@gmail.com>
2021-10-28 15:07:22 +0100

    Adding thirdContainer HomePage

---
+++ src/Components/HomePage/ThirdContainer/index.js
@@ -0,0 +1,23 @@
+import "./styles.scss";
+
+function ThirdContainer() {
+  return (
+    <div class="home__third-container">
+      <div class="home__third-container--right"></div>
+
+      <div class="home__third-container--left">
+        <h1 class="home--title">CONCERNANT NOTRE SAVOIR-FAIRE</h1>
+        <p class="home--text">
+          Spécialisés dans les décos intérieurs suédoises, nous sommes à même de
+          vous proposer des produits de qualités qui répondent parfaitement avec
+          vos attentes. Le savoir faire allié à l'élégance de nos meubles
+          scandinaves saura répondre à la moindre de vos envies. Toujours bien
+          pensé, vous retrouverez de page en page les éléments spécifiques à
+          cette décoration nordique.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ThirdContainer;
--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -1,11 +1,13 @@
 import MainContainer from "./MainContainer";
 import SecondContainer from "./SecondContainer";
+import ThirdContainer from "./ThirdContainer";
 
 function HomePage() {
   return (
     <div>
       <MainContainer />
       <SecondContainer />
+      <ThirdContainer />
     </div>
   );
 }
