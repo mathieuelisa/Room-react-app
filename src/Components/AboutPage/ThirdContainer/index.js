480435d49c2b17ed1056015bed45c3a07cce6bbf
mathieuelisa <mathieu.elisa@gmail.com>
2021-10-31 17:37:16 +0100

    Update

--- src/Components/AboutPage/ThirdContainer/index.js
+++ src/Components/AboutPage/ThirdContainer/index.js
@@ -7,15 +7,7 @@ function ThirdContainer() {
         <div className="picture-bloc"> </div>
       </div>
 
-      <div className="final-picture-bloc">
-        <div className="final-picture-bloc-citation">
-          <h2 className="final-picture-bloc-citation--title">
-            L'élégance, n'est pas n'est pas une question de luxe, de richesse.
-            Mais d'attitude....
-          </h2>
-          <p className="final-picture-bloc-citation--name">Mathieu Elisa</p>
-        </div>
-      </div>
+      <div className="final-picture-bloc"></div>
     </>
   );
 }
