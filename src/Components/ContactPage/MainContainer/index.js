86b4195f45a7875c23708dec438ee2b0ee2043da
mathieuelisa <mathieu.elisa@gmail.com>
2021-11-01 19:27:32 +0100

    Adding component Header to contact page

--- src/Components/ContactPage/MainContainer/index.js
+++ src/Components/ContactPage/MainContainer/index.js
@@ -1,8 +1,14 @@
+import Header from "../../HomePage/Header";
 import "./styles.scss";
 
 function MainContainer() {
   return (
     <div className="mainContainer__contact">
+      <Header
+        myMenu="myLink__menu"
+        myLink="myLink__menu-link"
+        myMenuLogo="myMenuLogo"
+      />
       <div className="mainContainer__contact-title">
         <h1>Contact</h1>
       </div>
