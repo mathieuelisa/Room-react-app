2dfd03a7ce8b53fcc3c54cab78904ae46343663f
mathieuelisa <mathieu.elisa@gmail.com>
2021-11-03 17:56:10 +0100

    Adding title

--- src/Components/ContactPage/Form/index.js
+++ src/Components/ContactPage/Form/index.js
@@ -27,78 +27,81 @@ function Form() {
   }
 
   return (
-    <form className="formulaire" onSubmit={handleSubmit}>
-      <div className="formulaire__input">
-        <label className="formulaire__label">Nom: </label>
-        <input
-          type="text"
-          className="formulaire__allInputs"
-          name="lastname"
-          value={myState.lastname}
-          onChange={handleChange}
-        />
-      </div>
+    <>
+      <h1 className="formulaire__mainTitle">FORMULAIRE</h1>
+      <form className="formulaire" onSubmit={handleSubmit}>
+        <div className="formulaire__input">
+          <label className="formulaire__label">Nom: </label>
+          <input
+            type="text"
+            className="formulaire__allInputs"
+            name="lastname"
+            value={myState.lastname}
+            onChange={handleChange}
+          />
+        </div>
 
-      <div className="formulaire__input">
-        <label className="formulaire__label">Prenom: </label>
-        <input
-          type="text"
-          className="formulaire__allInputs"
-          name="firstname"
-          value={myState.firstname}
-          onChange={handleChange}
-        />
-      </div>
+        <div className="formulaire__input">
+          <label className="formulaire__label">Prenom: </label>
+          <input
+            type="text"
+            className="formulaire__allInputs"
+            name="firstname"
+            value={myState.firstname}
+            onChange={handleChange}
+          />
+        </div>
 
-      <div className="formulaire__input">
-        <label className="formulaire__label">Telephone: </label>
-        <input
-          type="tel"
-          className="formulaire__allInputs"
-          name="phone"
-          value={myState.phone}
-          onChange={handleChange}
-        />
-      </div>
+        <div className="formulaire__input">
+          <label className="formulaire__label">Telephone: </label>
+          <input
+            type="tel"
+            className="formulaire__allInputs"
+            name="phone"
+            value={myState.phone}
+            onChange={handleChange}
+          />
+        </div>
 
-      <div className="formulaire__input">
-        <label className="formulaire__label">Email: </label>
-        <input
-          type="email"
-          className="formulaire__allInputs"
-          name="email"
-          value={myState.email}
-          onChange={handleChange}
-        />
-      </div>
+        <div className="formulaire__input">
+          <label className="formulaire__label">Email: </label>
+          <input
+            type="email"
+            className="formulaire__allInputs"
+            name="email"
+            value={myState.email}
+            onChange={handleChange}
+          />
+        </div>
 
-      <div className="formulaire__input">
-        <label className="formulaire__label">Magasin: </label>
-        <select name="shop" onChange={handleChange} value={myState.shop}>
-          <option>--Choissisez votre magasin--</option>
-          <option>Paris Sud - Evry</option>
-          <option>Paris Sud - Thiais</option>
-          <option>Paris Est - Villiers sur Marne</option>
-          <option>Paris Ouest - Plaisir</option>
-          <option>Paris Nord - Roissy</option>
-          <option>Lyon - Grand Parilly</option>
-        </select>
-      </div>
+        <div className="formulaire__input">
+          <label className="formulaire__label">Magasin: </label>
+          <select name="shop" onChange={handleChange} value={myState.shop}>
+            <option>--Choissisez votre magasin--</option>
+            <option>Paris Sud - Evry</option>
+            <option>Paris Sud - Thiais</option>
+            <option>Paris Est - Villiers sur Marne</option>
+            <option>Paris Ouest - Plaisir</option>
+            <option>Paris Nord - Roissy</option>
+            <option>Lyon - Grand Parilly</option>
+          </select>
+        </div>
 
-      <div className="formulaire__input">
-        <label className="formulaire__label">Commentaire: </label>
-        <textarea
-          type="text"
-          className="formulaire__textArea"
-          name="commentaires"
-          value={myState.commentaires}
-          onChange={handleChange}
-        />
-      </div>
-      <button className="formulaire__button-valide" type="submit">
-        Valide
-      </button>
-    </form>
+        <div className="formulaire__input">
+          <label className="formulaire__label">Commentaire: </label>
+          <textarea
+            type="text"
+            className="formulaire__textArea"
+            name="commentaires"
+            value={myState.commentaires}
+            onChange={handleChange}
+          />
+        </div>
+        <button className="formulaire__button-valide" type="submit">
+          Valide
+        </button>
+      </form>
+    </>
   );
 }
 
