0504c8d611fb46a6c52a429f4de45e79df4cd4ec
mathieuelisa <mathieu.elisa@gmail.com>
2021-11-12 19:00:17 +0100

    Adding title and CSS

--- src/Components/Products/Maincontainer/index.js
+++ src/Components/Products/Maincontainer/index.js
@@ -1,7 +1,22 @@
+import Header from "../../HomePage/Header";
 import "./styles.scss";
 
 function MainContainer() {
-  return <h1>Products</h1>;
+  return (
+    <div className="mainContainer__products">
+      <Header
+        myMenu="myLink__menu"
+        myLink="myLink__menu-linkProduct"
+        myMenuLogo="myMenuLogo"
+      />
+
+      <div className="mainContainer__products-title">
+        <div className="mainContainer__products-productsTitle">
+          <h2 className="mainProducts__title">INDOOR</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MainContainer;
