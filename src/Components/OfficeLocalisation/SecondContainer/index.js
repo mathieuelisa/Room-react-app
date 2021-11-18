9d870ce802f47ea639857b6daa75eba0437c1ac1
mathieuelisa <mathieu.elisa@gmail.com>
2021-11-18 22:54:44 +0100

    Adding informations OfficeLocation page

--- src/Components/OfficeLocalisation/SecondContainer/index.js
+++ src/Components/OfficeLocalisation/SecondContainer/index.js
@@ -3,7 +3,26 @@ import "./styles.scss";
 function SecondContainer() {
   return (
     <div className="location__second-container">
-      <div className="location__second-container-general"></div>
+      <div className="location__second-container-general">
+        <div className="location__second-container-text">
+          <h3 className="store__detail-title">ROOM HEADQUARTERS</h3>
+          <p>178 rue sebastien</p>
+          <p className="store__detail-adress">75011 PARIS</p>
+
+          <p>Lundi - Vendredi</p>
+          <p className="store__detail-hours">9h - 13h / 14h - 19h</p>
+
+          <p>Samedi</p>
+          <p className="store__detail-hours">9h - 12h</p>
+
+          <p className="store__detail-phoneNumber">09 87 66 88 38</p>
+          <p className="store__detail-mail">room@service-client.com</p>
+        </div>
+
+        <div className="location__second-container-map">
+          <p>Map</p>
+        </div>
+      </div>
     </div>
   );
 }
