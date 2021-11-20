6b7e1ce7e87337c9d1516587457c24aebd9800da
mathieuelisa <mathieu.elisa@gmail.com>
2021-11-20 22:29:07 +0100

    Adding text for mentions

--- src/Components/MentionsLegales/SecondContainer/index.js
+++ src/Components/MentionsLegales/SecondContainer/index.js
@@ -1,5 +1,40 @@
 function SecondContainer() {
-  return <h1>SecondContainer</h1>;
+  return (
+    <div>
+      <h3 className="mentionsContainer__container-bloc-sectionTitle">
+        Éditeurs du Site :
+      </h3>
+      <p>
+        L’édition et la direction de la publication du site
+        https://letsbefriends.netlify.app est assurée par ELISA Mathieu, MAURICE
+        Julien, MARIEN Robin, ESPITALIER Jordan et MARTIN Emmanuel.
+      </p>
+
+      <h3 className="mentionsContainer__container-bloc-sectionTitle">
+        Hébergement :
+      </h3>
+      <p>
+        Hébergeur : NETLIFY 2325 3RD STREET,SUITE 215, SAN FRANCISCO, CA 94107
+        USA.
+      </p>
+
+      <h3 className="mentionsContainer__container-bloc-sectionTitle">
+        Conditions d’utilisation :
+      </h3>
+      <p>
+        Ce site (letsbefriends.netlify.app) est proposé en différents langages
+        web (HTML, HTML5, Javascript, CSS, etc…) pour un meilleur confort
+        d’utilisation et un graphisme plus agréable. Nous vous recommandons de
+        recourir à des navigateurs modernes comme Internet explorer, Safari,
+        Firefox, Google Chrome, etc… Toutefois, des erreurs ou omissions peuvent
+        survenir. L’internaute devra donc s’assurer de l’exactitude des
+        informations auprès de l'equipe de LETS BE FRIENDS , et signaler toutes
+        modifications du site qu’il jugerait utile. Lets be Friends n’est en
+        aucun cas responsable de l’utilisation faite de ces informations, et de
+        tout préjudice direct ou indirect pouvant en découler.
+      </p>
+    </div>
+  );
 }
 
 export default SecondContainer;
