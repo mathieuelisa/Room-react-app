74241c115703d73902e4ae39797d5cda0c8c6f98
mathieuelisa <mathieu.elisa@gmail.com>
2021-11-20 22:30:36 +0100

    Adding text for mentions

--- src/Components/MentionsLegales/SecondContainer/index.js
+++ src/Components/MentionsLegales/SecondContainer/index.js
@@ -33,6 +33,45 @@ function SecondContainer() {
         aucun cas responsable de l’utilisation faite de ces informations, et de
         tout préjudice direct ou indirect pouvant en découler.
       </p>
+
+      <h3 className="mentionsContainer__container-bloc-sectionTitle">
+        Cookies :
+      </h3>
+      <p>
+        Le site letsbefriends.netlify.app peut-être amené à vous demander
+        l’acceptation des cookies pour des besoins de statistiques et
+        d’affichage. Un cookie est une information déposée sur votre disque dur
+        par le serveur du site que vous visitez. Il contient plusieurs données
+        qui sont stockées sur votre ordinateur dans un simple fichier texte
+        auquel un serveur accède pour lire et enregistrer des informations.
+        Certaines parties de ce site ne peuvent être fonctionnelles sans
+        l’acceptation de cookies.
+      </p>
+
+      <h3 className="mentionsContainer__container-bloc-sectionTitle">
+        Liens hypertextes :
+      </h3>
+      <p>
+        Les sites internet de peuvent offrir des liens vers d’autres sites
+        internet ou d’autres ressources disponibles sur Internet. LETS BE
+        FRIENDS ne dispose d’aucun moyen pour contrôler les sites en connexion
+        avec ses sites internet. Lets be Friends ne répond pas de la
+        disponibilité de tels sites et sources externes, ni ne la garantit. Elle
+        ne peut être tenue pour responsable de tout dommage, de quelque nature
+        que ce soit, résultant du contenu de ces sites ou sources externes, et
+        notamment des informations, produits ou services qu’ils proposent, ou de
+        tout usage qui peut être fait de ces éléments. Les risques liés à cette
+        utilisation incombent pleinement à l’internaute, qui doit se conformer à
+        leurs conditions d’utilisation. Les utilisateurs, les abonnés et les
+        visiteurs des sites internet ne peuvent pas mettre en place un hyperlien
+        en direction de ce site sans l’autorisation expresse et préalable de
+        LETS BE FRIENDS. Dans l’hypothèse où un utilisateur ou visiteur
+        souhaiterait mettre en place un hyperlien en direction d’un des sites
+        internet letsbefriends.netlify.app, il lui appartiendra d’adresser un
+        email accessible sur le site afin de formuler sa demande de mise en
+        place d’un hyperlien. LETS BE FRIENDS se réserve le droit d’accepter ou
+        de refuser un hyperlien sans avoir à en justifier sa décision.
+      </p>
     </div>
   );
 }
