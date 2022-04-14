00d39220f36c20e925d7bc4f2bd23b10445fb1d4
mathieuelisa <mathieu.elisa@gmail.com>
2022-04-14 18:02:00 +0200

    Structure of css new feature

--- src/Components/Questions/SecondContainer/index.js
+++ src/Components/Questions/SecondContainer/index.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import dataQuestionsAnswers from "../../../data.js";
+
+import "./styles.scss";
+
 function SecondContainer() {
+  const [clicked, setClicked] = useState(false);
+
+  const toggle = (index) => {
+    if (clicked === index) {
+      return setClicked(null);
+    }
+    setClicked(index);
+  };
+
   return (
-    <div>
-      <h1>Page de questions reponses</h1>
+    <div className="questions__container">
+      {dataQuestionsAnswers.map((element, index) => (
+        <div
+          key={index}
+          className="questions__container-each"
+          onClick={() => toggle(index)}
+        >
+          <p>{element.questions}</p>
+          {clicked === index ? (
+            <div className="questions__container-answers">
+              <p>{element.answer}</p>
+            </div>
+          ) : null}
+        </div>
+      ))}
     </div>
   );
 }
